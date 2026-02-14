404313093a7709dd3d9d2251d987bb3934112e29
itsabduldeveloper <itsabduldeveloper@gmail.com>
2026-02-14 23:20:51 +0500

    okay

--- Backend-Server-Startup/server.js
+++ Backend-Server-Startup/server.js
@@ -17,6 +17,9 @@ app.get('/', (req, res)=>{
 app.get('/about', (req, res)=>{
     res.send('About Page')
 })
+app.get("/product", (req, res)=>{
+    res.send("Product Page")
+})
 app.get("/contact", (req, res)=>{
     res.send("Contact Page")
 })
