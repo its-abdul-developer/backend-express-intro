dbe86cf5ed35a58e9b35007cfeffdfb0f1ac247e
itsabduldeveloper <itsabduldeveloper@gmail.com>
2026-02-16 22:59:03 +0500

    completed

--- Backend-Server-Startup/server.js
+++ Backend-Server-Startup/server.js
@@ -11,7 +11,7 @@ app.listen(3000)
 
 // Make some routes and send the response to the client
 app.get('/', (req, res)=>{
-    //req is used to get the request from the frontend and res is used to send the response to the frontend
+//req is used to get the request from the frontend and res is used to send the response to the frontend
     res.send('Hello World')
 })
 
@@ -25,3 +25,4 @@ app.get("/contact", (req, res)=>{
     res.send("Contact Page")
 })
 
+// now api staret the server using node server.js in the terminal and open the browser and type http://localhost:3000/ to see the response from the server. You can also type http://localhost:3000/about, http://localhost:3000/product, http://localhost:3000/contact to see the response from the server for different routes.
